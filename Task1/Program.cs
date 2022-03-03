5b35a694f686aeef382d8a956683d3a6d9396a84
Alex <virdata@yandex.ru>
2022-03-03 23:07:02 +0800

    Задание 2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -8,14 +8,74 @@ namespace Task1
 {
     class Program
     {
+        //enum months
+        //{
+        //    январь,
+        //    февраль,
+        //    март,
+        //    апрель,
+        //    май,
+        //    июнь,
+        //    июль,
+        //    август,
+        //    сентябрь,
+        //    октябрь,
+        //    ноябрь,
+        //    декабрь
+        //}
         static void Main(string[] args)
         {
             Console.Write("Введите минимальную температуру: ");
             int tmin = Convert.ToInt32(Console.ReadLine());
             Console.Write("Введите максимальную температуру: ");
             int tmax = Convert.ToInt32(Console.ReadLine());
-            int middle = (tmax + tmin) / 2;
-            Console.WriteLine($"Среднесуточная температура: {middle}");
+            int tmiddle = (tmax + tmin) / 2;
+            Console.WriteLine($"Среднесуточная температура: {tmiddle}");
+
+            Console.Write("Введите порядковый номер месяца: ");           
+            byte numbmonth = Convert.ToByte(Console.ReadLine());
+            switch (numbmonth)
+            {
+                case 1: 
+                    Console.WriteLine("Январь");
+                    break;
+                case 2: 
+                    Console.WriteLine("Февраль");
+                    break;
+                case 3: 
+                    Console.WriteLine("Март");
+                    break;
+                case 4: 
+                    Console.WriteLine("Апрель");
+                    break;
+                case 5: 
+                    Console.WriteLine("Май");
+                    break;
+                case 6: 
+                    Console.WriteLine("Июнь");
+                    break;
+                case 7: 
+                    Console.WriteLine("Июль");
+                    break;
+                case 8: 
+                    Console.WriteLine("Август");
+                    break;
+                case 9: 
+                    Console.WriteLine("Сентябрь");
+                    break;
+                case 10: 
+                    Console.WriteLine("Октябрь");
+                    break;
+                case 11: 
+                    Console.WriteLine("Ноябрь");
+                    break;
+                case 12: 
+                    Console.WriteLine("Декабрь");
+                    break;
+                default:
+                    Console.WriteLine("Не верно указан номер месяца");
+                    break;
+            }
         }    
     }
 }
